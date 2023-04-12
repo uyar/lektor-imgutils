947bcb725e69633b447532d2bfb93c80f41b0016
H. Turgut Uyar <uyar@tekir.org>
2023-04-13 01:52:56 +0300

    Add configuration options to manage operations

--- lektor_imgutils.py
+++ lektor_imgutils.py
@@ -5,40 +5,75 @@ from lektor.pluginsystem import Plugin
 # from lektor.reporter import reporter
 from PIL import Image
 
-IMG_DATA = {}
-
 
 class ImgUtilsPlugin(Plugin):
-    name = "lektor-imgutils"
+    name = "imgutils"
     description = "Image handling utilities."
+
+    def __init__(self, *args, **kwargs):
+        Plugin.__init__(self, *args, **kwargs)
+        config = self.get_config()
+
+        self.sections = {}
+        for section in config.sections():
+            self.sections[section] = {
+                "selector": config.get(f"{section}.selector"),
+                "size": config.get_bool(f"{section}.size", False),
+                "loading": config.get(f"{section}.loading"),
+                "decoding": config.get(f"{section}.decoding"),
+            }
+
+        self.images = {}
 
     def on_after_build_all(self, builder, **extra):
         # reporter.report_generic("image utilities started")
-        for html_file in Path(builder.destination_path).glob("**/*.html"):
-            modified = False
-            content = html_file.read_text()
-            lines = content.splitlines()
+        for page in Path(builder.destination_path).glob("**/*.html"):
+            content = page.read_text()
             soup = BeautifulSoup(content, "html.parser")
-            for img_tag in soup.find_all("img"):
-                img_attrs = img_tag.attrs
-                img_file = html_file.parent.joinpath(img_tag["src"]).resolve()
-                if ("width" not in img_attrs) and ("height" not in img_attrs):
-                    img_data = IMG_DATA.get(img_file, {})
-                    if len(img_data) == 0:
-                        with Image.open(img_file) as img:
-                            img_data["width"] = img.width
-                            img_data["height"] = img.height
-                        IMG_DATA[img_file] = img_data
+            modified = False
 
-                    line = lines[img_tag.sourceline - 1]
-                    assert line[img_tag.sourcepos] == "<", line
-                    start_pos = line.index("i")
-                    assert line[start_pos:start_pos + 3] == "img", line
-                    assert line[start_pos + 3].isspace(), line
-                    new_attrs = 'width="%(width)s" height="%(height)s"' % img_data
-                    new_line = line[:start_pos + 4] + new_attrs + line[start_pos + 3:]
-                    lines[img_tag.sourceline - 1] = new_line
+            added_attrs = {}
+            for options in self.sections.values():
+                for tag in soup.select(options["selector"]):
+                    img_file = page.parent.joinpath(tag["src"]).resolve()
+                    if img_file not in self.images:
+                        self.images[img_file] = {}
+                    if tag not in added_attrs:
+                        added_attrs[tag] = {}
+
+                    sized = ("width" in tag.attrs) or ("height" in tag.attrs)
+                    if options["size"] and (not sized):
+                        img_data = self.images[img_file]
+                        if ("width" not in img_data) or ("height" not in img_data):
+                            with Image.open(img_file) as img:
+                                img_data["width"] = img.width
+                                img_data["height"] = img.height
+                        added_attrs[tag]["width"] = img_data["width"]
+                        added_attrs[tag]["height"] = img_data["height"]
+
+                    loading = options.get("loading")
+                    if (loading is not None) and ("loading" not in tag.attrs):
+                        added_attrs[tag]["loading"] = loading
+
+                    decoding = options.get("decoding")
+                    if (decoding is not None) and ("decoding" not in tag.attrs):
+                        added_attrs[tag]["decoding"] = decoding
+
+            lines = content.splitlines()
+            for tag in soup.find_all("img"):
+                img_file = page.parent.joinpath(tag["src"]).resolve()
+                added = added_attrs.get(tag, {})
+                if len(added) > 0:
+                    line_no = tag.sourceline - 1
+                    line = lines[line_no]
+                    pos = line.index("i", tag.sourcepos) + 3
+                    assert line[pos - 3:pos] == "img", line
+                    assert line[pos].isspace(), line
+
+                    add_str = " ".join(f'{k}="{v}"' for k, v in added.items())
+                    lines[line_no] = line[:pos] + " " + add_str + line[pos:]
                     modified = True
+
             if modified:
-                html_file.write_text('\n'.join(lines))
+                page.write_text('\n'.join(lines))
         # reporter.report_generic("image utilities finished")
