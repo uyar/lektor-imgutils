f1981b24d1b376ccebe57d90d19f9d61e66b2415
H. Turgut Uyar <uyar@tekir.org>
2024-05-06 01:44:17 +0300

    Switch from beautifulsoup to lxml

--- lektor_imgutils.py
+++ lektor_imgutils.py
@@ -3,15 +3,21 @@
 # lektor-imgutils is released under the BSD license.
 # Read the included LICENSE.txt file for details.
 
+from functools import partial
 from pathlib import Path
 
-from bs4 import BeautifulSoup
 from lektor.pluginsystem import Plugin
 from lektor.reporter import reporter
+from lxml import html
+from lxml.cssselect import CSSSelector
 from PIL import Image, UnidentifiedImageError
 
 
 IMG_ATTRS = {"decoding", "loading"}
+
+
+from_html = html.fromstring
+to_html = partial(html.tostring, encoding="unicode")
 
 
 class ImgUtilsPlugin(Plugin):
@@ -19,10 +25,12 @@ class ImgUtilsPlugin(Plugin):
     description = "Image handling utilities."
 
     def on_setup_env(self, **extra):
-        self.ops = {}
         config = self.get_config()
+
+        self.ops = {}
         for section in config.sections():
-            selector = config.pop(f"{section}.selector")
+            selector_expr = config.pop(f"{section}.selector")
+            selector = CSSSelector(selector_expr)
             if selector not in self.ops:
                 self.ops[selector] = {}
 
@@ -33,20 +41,21 @@ class ImgUtilsPlugin(Plugin):
 
             options = config.section_as_dict(section)
             for attr, value in options.items():
-                if attr in IMG_ATTRS:
-                    self.ops[selector][attr] = value
+                if attr not in IMG_ATTRS:
+                    raise ValueError(f"Unknown image property: {attr}")
+                self.ops[selector][attr] = value
 
-        self.images = {}
+        self.image_data = {}
         self.skip_images = set()
 
     def get_size(self, image_path):
-        image_data = self.images[image_path]
+        image_data = self.image_data[image_path]
         if ("width" not in image_data) or ("height" not in image_data):
             image = None
             try:
                 image = Image.open(image_path)
-                image_data["width"] = image.width
-                image_data["height"] = image.height
+                image_data["width"] = str(image.width)
+                image_data["height"] = str(image.height)
             finally:
                 if image is not None:
                     image.close()
@@ -54,50 +63,33 @@ class ImgUtilsPlugin(Plugin):
 
     def on_after_build_all(self, builder, **extra):
         reporter.report_generic("Starting image utilities")
-
         for page in Path(builder.destination_path).glob("**/*.html"):
             content = page.read_text()
-            soup = BeautifulSoup(content, "html.parser")
-
-            added_attrs = {}
+            tree = from_html(content)
+            modified = False
             for selector, ops in self.ops.items():
                 set_size = ops.pop("set_size", False)
-                for tag in soup.select(selector):
-                    image_path = page.parent.joinpath(tag["src"]).resolve()
+                for tag in selector(tree):
+                    src = tag.attrib["src"]
+                    image_path = page.parent.joinpath(src).resolve()
                     if image_path in self.skip_images:
                         continue
-                    if image_path not in self.images:
-                        self.images[image_path] = {}
-                    if tag not in added_attrs:
-                        added_attrs[tag] = {}
-
-                    if set_size and ("width" not in tag.attrs) and ("height" not in tag.attrs):
+                    if image_path not in self.image_data:
+                        self.image_data[image_path] = {}
+                    if set_size and ("width" not in tag.attrib) and \
+                        ("height" not in tag.attrib):
                         try:
                             size_attrs = self.get_size(image_path)
                             if len(size_attrs) > 0:
-                                added_attrs[tag].update(size_attrs)
+                                tag.attrib.update(size_attrs)
+                                modified = True
                         except UnidentifiedImageError:
                             self.skip_images.add(image_path)
-                            del self.images[image_path]
+                            del self.image_data[image_path]
                             continue
-
                     for attr, value in ops.items():
-                        if attr not in tag.attrs:
-                            added_attrs[tag][attr] = value
-
-            modified = False
-            lines = content.splitlines()
-            for tag in soup.find_all("img"):
-                added = added_attrs.get(tag, {})
-                if len(added) > 0:
-                    line_no = tag.sourceline - 1
-                    line = lines[line_no]
-                    pos = line.index("img", tag.sourcepos) + 3
-                    add_str = " ".join(f'{k}="{v}"' for k, v in added.items())
-                    lines[line_no] = line[:pos] + " " + add_str + line[pos:]
-                    modified = True
-
+                        if attr not in tag.attrib:
+                            tag.attrib[attr] = value
             if modified:
-                page.write_text('\n'.join(lines))
-
+                page.write_text(to_html(tree))
         reporter.report_generic("Finished image utilities")
