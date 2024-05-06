2054c0caf69c636b7386ed586aaf96e36d4d0255
H. Turgut Uyar <uyar@tekir.org>
2024-05-07 01:03:32 +0300

    Use typedload to parse configuration

--- lektor_imgutils.py
+++ lektor_imgutils.py
@@ -3,93 +3,107 @@
 # lektor-imgutils is released under the BSD license.
 # Read the included LICENSE.txt file for details.
 
+from dataclasses import dataclass
 from functools import partial
 from pathlib import Path
+from typing import Dict, List, Literal, Set, Tuple, Union
 
+import typedload
+from lektor.builder import Builder
 from lektor.pluginsystem import Plugin
 from lektor.reporter import reporter
 from lxml import html
 from lxml.cssselect import CSSSelector
+from lxml.etree import _Element as Element
 from PIL import Image, UnidentifiedImageError
-
-
-IMG_ATTRS = {"decoding", "loading"}
 
 
 from_html = html.fromstring
 to_html = partial(html.tostring, encoding="unicode")
 
 
+@dataclass
+class ImageAttributes:
+    width: Union[int, Literal["intrinsic"], None] = None
+    height: Union[int, Literal["intrinsic"], None] = None
+    loading: Union[Literal["eager", "lazy"], None] = None
+    decoding: Union[Literal["sync", "async", "auto"], None] = None
+
+
 class ImgUtilsPlugin(Plugin):
     name = "imgutils"
     description = "Image handling utilities."
 
-    def on_setup_env(self, **extra):
+    def on_setup_env(self, **extra) -> None:
         config = self.get_config()
+        self.ops: Dict[CSSSelector, ImageAttributes] = {}
+        for selector in config.sections():
+            data = config.section_as_dict(selector)
+            parsed = typedload.load(data, ImageAttributes, failonextra=True)
+            self.ops[CSSSelector(selector)] = parsed
+        self.image_data: Dict[Path, ImageAttributes] = {}
+        self.skip_images: Set[Path] = set()
 
-        self.ops = {}
-        for section in config.sections():
-            selector_expr = config.pop(f"{section}.selector")
-            selector = CSSSelector(selector_expr)
-            if selector not in self.ops:
-                self.ops[selector] = {}
+    def get_intrinsic_size(self, path: Path) -> Tuple[int, int]:
+        data = self.image_data.get(path)
+        if data is None:
+            data = ImageAttributes()
+            self.image_data[path] = data
+        if (data.width is None) or (data.height is None):
+            with Image.open(path) as image:
+                data.width = image.width
+                data.height = image.height
+        return (data.width, data.height)
 
-            set_size = config.get_bool(f"{section}.set_size", None)
-            if set_size is not None:
-                self.ops[selector]["set_size"] = set_size
-                config.pop(f"{section}.set_size")
-
-            options = config.section_as_dict(section)
-            for attr, value in options.items():
-                if attr not in IMG_ATTRS:
-                    raise ValueError(f"Unknown image property: {attr}")
-                self.ops[selector][attr] = value
-
-        self.image_data = {}
-        self.skip_images = set()
-
-    def get_size(self, image_path):
-        image_data = self.image_data[image_path]
-        if ("width" not in image_data) or ("height" not in image_data):
-            image = None
-            try:
-                image = Image.open(image_path)
-                image_data["width"] = str(image.width)
-                image_data["height"] = str(image.height)
-            finally:
-                if image is not None:
-                    image.close()
-        return {"width": image_data["width"], "height": image_data["height"]}
-
-    def on_after_build_all(self, builder, **extra):
+    def on_after_build_all(self, builder: Builder, **extra) -> None:
         reporter.report_generic("Starting image utilities")
-        for page in Path(builder.destination_path).glob("**/*.html"):
+        dst_root: str = builder.destination_path
+        for page in Path(dst_root).glob("**/*.html"):
             content = page.read_text()
             tree = from_html(content)
             modified = False
             for selector, ops in self.ops.items():
-                set_size = ops.pop("set_size", False)
-                for tag in selector(tree):
-                    src = tag.attrib["src"]
+                tags: List[Element] = selector(tree)
+                for tag in tags:
+                    atts = tag.attrib
+                    src = atts.get("src")
+                    if src is None:
+                        continue
                     image_path = page.parent.joinpath(src).resolve()
                     if image_path in self.skip_images:
                         continue
-                    if image_path not in self.image_data:
-                        self.image_data[image_path] = {}
-                    if set_size and ("width" not in tag.attrib) and \
-                        ("height" not in tag.attrib):
-                        try:
-                            size_attrs = self.get_size(image_path)
-                            if len(size_attrs) > 0:
-                                tag.attrib.update(size_attrs)
-                                modified = True
-                        except UnidentifiedImageError:
-                            self.skip_images.add(image_path)
-                            del self.image_data[image_path]
-                            continue
-                    for attr, value in ops.items():
-                        if attr not in tag.attrib:
-                            tag.attrib[attr] = value
+
+                    if (ops.width is not None) and ("width" not in atts):
+                        if ops.width == "intrinsic":
+                            try:
+                                width, _ = self.get_intrinsic_size(image_path)
+                            except UnidentifiedImageError:
+                                self.skip_images.add(image_path)
+                                continue
+                        else:
+                            width = ops.width
+                        atts["width"] = str(width)
+                        modified = True
+
+                    if (ops.height is not None) and ("height" not in atts):
+                        if ops.height == "intrinsic":
+                            try:
+                                _, height = self.get_intrinsic_size(image_path)
+                            except UnidentifiedImageError:
+                                self.skip_images.add(image_path)
+                                continue
+                        else:
+                            height = ops.height
+                        atts["height"] = str(height)
+
+                    if (ops.loading is not None) and ("loading" not in atts):
+                        atts["loading"] = ops.loading
+                        modified = True
+
+                    if (ops.decoding is not None) and ("decoding" not in atts):
+                        atts["decoding"] = ops.decoding
+                        modified = True
+
             if modified:
                 page.write_text(to_html(tree))
         reporter.report_generic("Finished image utilities")
